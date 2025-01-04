9646775bee5a65ff5a04e8e7851bda2ba0b199fd
carsonwolber <ctw54@cornell.edu>
2025-01-04 03:01:21 -0500

    task 0.1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -10,21 +10,60 @@ from typing import Callable, Iterable
 
 # Mathematical functions:
 # - mul
+def mul(a: float, b:float):
+  return a * b 
 # - id
+def id(a): # type: ignore
+  return a
 # - add
+def add(a: float, b:float):
+  return a + b
 # - neg
+def neg(a: float):
+  return -1 * a
 # - lt
+def lt(a: float, b:float):
+  return a < b
 # - eq
+def eq(a: float, b:float):
+  return a == b
 # - max
+def max(a: float, b:float):
+  if a > b :
+    return a
+  return b 
 # - is_close
+def is_close(a: float, b: float):
+  return abs(a - b) < .01
 # - sigmoid
+def sigmoid(x: float):
+  if x >= 0:
+    return 1.0 / (1.0 + math.e ** -x)
+  return math.e**x / (1.0 + math.e**x)
 # - relu
+def relu(x: float):
+  return max(x, 0)
 # - log
+def log(x: float):
+  return math.log(x, math.e)
 # - exp
+def exp(x:float):
+  return math.e ** x
 # - log_back
+def log_back(a: float, b: float):
+  return 1/a * b
 # - inv
+def inv(a: float):
+  return 1/a
 # - inv_back
+def inv_back(a: float, b:float):
+  (-1/a**2) * b 
 # - relu_back
+def relu_back(x: float, y:float):
+  if max(x,0) == x:
+    return y
+  return 0
+  
 #
 # For sigmoid calculate as:
 # $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
